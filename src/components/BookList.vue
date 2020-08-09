4bcef149b055e0e05bd9d4fb38eb68c7186d2e09
Nidhi <nidhichandu@gmail.com>
2020-08-09 13:08:23 -0500

    Completed module

---
+++ src/components/BookList.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <h1>{{title}}</h1>
+</div>
+</template>
+
+<script>
+module.exports = {
+    name: 'BookList',
+    data: function () {
+        return {
+            title: 'All Books'
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1,h2 {
+    font-weight: normal
+}
+</style>
